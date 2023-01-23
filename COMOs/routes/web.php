314167789f1cab28063d83b13bf61407ab713a0b
blessyou9 <121041603@upq.edu.mx>
2023-01-23 16:36:59 -0600

    Udate

--- COMOs/routes/web.php
+++ COMOs/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControladorPaginas;
 
 
-Route ::get('/',[ControladorPaginas::class,'fInicio'])->name('NInicio');
+Route ::get('/',[ControladorPaginas::class,'_invoke'])->name('NInicio');
+Route ::get('/inicio',[ControladorPaginas::class,'fInicio'])->name('NInicio');
 Route::get('/contact', [ControladorPaginas::class,'fContact'])->name('NContact');
 Route::post('/contact_post', [ControladorPaginas::class,'contact_post']);
-//Route::post('/contact_post', 'HomeController@contact_post');
